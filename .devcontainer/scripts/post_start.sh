5f542b9c92c64ac8b775bb6140a3f6f87f22db37
223n <223n@223n.tech>
2025-02-14 16:15:58 +0000

    ログファイルを生成するように修正

--- .devcontainer/scripts/post_start.sh
+++ .devcontainer/scripts/post_start.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# フォルダの管理者がrootになってしまう問題に対応
-sudo chown $(whoami):$(whoami) -R .
+# メイン処理
+main() {
+    # フォルダの管理者がrootになってしまう問題に対応
+    sudo chown $(whoami):$(whoami) -R .
+}
+
+# ログフォルダの生成
+mkdir -p /workspace/log
+
+# スクリプトの実行
+main "$@" 2>&1 | tee -a "/workspace/log/$(date +'%Y-%m-%d-%H-%M-%S')_devcontainer-start.log"
